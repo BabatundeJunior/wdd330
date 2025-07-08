c04e831a9822c2a33f3359668cf6a0be5bc7aaa4
BabatundeJunior <jbabs128@gmail.com>
2025-07-08 02:49:59 +0100

    .

--- src/js/product.js
+++ src/js/product.js
@@ -4,28 +4,18 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  // Retrieves existing cart from localStorage
-  let cart = getLocalStorage("so-cart");
-
-  // Ensures cart is an array, or reset it
-  if (!Array.isArray(cart)) {
-    cart = [];
-  }
-
-  // Adds a new product to cart array
-  cart.push(product);
-
-  // Saves updated cart back to localStorage
-  setLocalStorage("so-cart", cart);
+  const cartItems = getLocalStorage("so-cart") || []; // get cart array of items from local storage if null set to empty array
+  cartItems.push(product);
+  setLocalStorage("so-cart", cartItems);
 }
 
-// Add to Cart button event handler
+// add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
   addProductToCart(product);
 }
 
-// listener for Add to Cart button
+// add listener to Add to Cart button
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);
